161a7029ff547cf28479b549c137f07020b26955
Tomi Caratti <tomircaratti@gmail.com>
2024-03-17 20:12:23 -0300

    create landing page & login component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Login from './pages/Login/Login'
-import Register from './pages/Login/Register'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import LandingPage from './pages/landing/LandingPage.jsx'
+import Home from './pages/landing/Home.jsx'
+import Login from './pages/landing/Login.jsx'
+import Register from './pages/landing/Register.jsx'
 
 function App() {
 
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-    </Routes>
+      <Route path='/' element={<LandingPage />}>
+        <Route index element={<Home />} />
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+      </Route>
+      <Route path='*' element={<Navigate to="/" />} />
+    </Routes >
   )
 }
 
---
+++ src/pages/landing/Register.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Register() {
+  return (
+    <>
+    <div>Register</div>
+    <Link to='/'>Regresar</Link>
+    </>
+  )
+}
+
+export default Register
