34916f54746134facb285e595dbe42a35de7cc44
Tomi Caratti <tomircaratti@gmail.com>
2024-03-18 10:38:21 -0300

    merge landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,8 @@
 
-import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Login from './pages/Login/Login'
-import Register from './pages/Login/Register'
-
 function App() {
 
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-    </Routes>
+    <div>hola</div>
   )
 }
 
